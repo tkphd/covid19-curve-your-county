b215ad5e4b94709265ae1f37fbc166a1cf39a46a
Trevor Keller <trevor.keller@nist.gov>
2020-03-23 17:31:53 -0400

    fully ported model from R to Python

--- exponential.py
+++ exponential.py
@@ -6,38 +6,125 @@ import numpy as np
 import pandas as pd
 from kapteyn import kmpfit
 from time import strptime
+from datetime import date
+from scipy.stats import t
 
 def model(p, x):
-    a, b, c = p
-    return a * np.exp(b * x) + c
+    a, b = p
+    return a * (1 + b) ** x
 
+
+fig = plt.figure(figsize=(6, 4))
+plt.suptitle("COVID-19 Cases: Montgomery County, MD")
+plt.title("github.com/tkphd/covid19-curve-your-county")
+plt.xlabel("# Days")
+plt.ylabel("# Diagnosed Cases")
+
+# Data
 
 df = pd.read_csv("us_md_montgomery.csv")
 
 y = np.array(df["diagnosed"])
 start = strptime(df["date"].iloc[0], "%Y-%m-%d").tm_yday
+today = strptime(df["date"].iloc[-1], "%Y-%m-%d").tm_yday
 
 x = np.zeros_like(y)
 for i in range(len(x)):
     x[i] = strptime(df["date"].iloc[i], "%Y-%m-%d").tm_yday - start
 
-# fit
-f = kmpfit.simplefit(model, [1, 1, 1], x, y)
-a, b, c = f.params
-cov = f.covar
-print("cases ~ {0:.2g} * exp({1:.2g} * (t - t0)) - {2:.2g}".format(a, b, -c))
+plt.scatter(x, y, marker=".", s=10, color="k", zorder=10)
 
-# confidence band; dfdp represents the partial derivatives of the model with respect to each parameter p (i.e., a, b, and c)
-dfdp = [np.exp(b * x), a * x * np.exp(b * x), 1]
-yhat, upper, lower = f.confidence_band(x, dfdp, 0.99, model)
+# Levenburg-Marquardt Least-Squares Fit
 
-plt.scatter(x, y, marker=".", s=10, color="#0000ba")
-ix = np.argsort(x)
-for i, l in enumerate((upper, lower, yhat)):
-    plt.plot(x[ix], l[ix], c="g" if i == 2 else "r", lw=2)
+f = kmpfit.simplefit(model, [1, 1], x, y)
+a, b = f.params
 
+print("cases ~ {0:.2g} * (1 + {1:.2g})^(t - t0))".format(a, b))
 
-plt.savefig("exponential.png", dpi=400, bbox_inches="tight")
+# Confidence Band: dfdp represents the partial derivatives of the model with respect to each parameter p (i.e., a, b, and c)
 
-print("Prediction, 1 day  from now: {0:.0f}".format(a * np.exp(b * (x[-1]+1)) + c))
-print("Prediction, 1 week from now: {0:.0f}".format(a * np.exp(b * (x[-1]+7)) + c))
+xhat = np.linspace(0, x[-1] + 7, 100)
+dfdp = [(1 + b) ** xhat, (a * xhat * (1 + b) ** xhat) / (1 + b)]
+level = 0.95
+yhat, upper, lower = f.confidence_band(xhat, dfdp, level, model)
+
+ix = np.argsort(xhat)
+plt.plot(xhat[ix], yhat[ix], c="red", lw=1, zorder=5)
+plt.fill_between(
+    xhat[ix], upper[ix], yhat[ix], edgecolor=None, facecolor="silver", zorder=1
+)
+plt.fill_between(
+    xhat[ix], lower[ix], yhat[ix], edgecolor=None, facecolor="silver", zorder=1
+)
+
+# Plot Boundaries
+
+plt.xlim([0, xhat[-1]])
+plt.ylim([0, upper[-1]])
+
+# Predictions
+
+tomorrow = date.fromordinal(today + 1)
+nextWeek = date.fromordinal(today + 7)
+
+xhat = np.array([tomorrow.toordinal() - start, nextWeek.toordinal() - start])
+dfdp = [(1 + b) ** xhat, (a * xhat * (1 + b) ** xhat) / (1 + b)]
+yhat, upper, lower = f.confidence_band(xhat, dfdp, level, model)
+dx = 0.25
+
+plt.text(
+    dx,
+    yhat[0],
+    "{0}/{1}: ({2:.0f} < {3:.0f} < {4:.0f})".format(
+        tomorrow.month, tomorrow.day, lower[0], yhat[0], upper[0]
+    ),
+    va="center",
+    zorder=5,
+    bbox=dict(boxstyle="round", ec="black", fc="white", linewidth=dx),
+)
+plt.text(
+    dx,
+    yhat[1],
+    "{0}/{1}: ({2:.0f} < {3:.0f} < {4:.0f})".format(
+        nextWeek.month, nextWeek.day, lower[1], yhat[1], upper[1]
+    ),
+    va="center",
+    zorder=5,
+    bbox=dict(boxstyle="round", ec="black", fc="white", linewidth=dx),
+)
+
+hw = (upper[1] - lower[1]) / 50
+hl = xhat[1] / 100
+
+plt.arrow(
+    dx,
+    yhat[0],
+    xhat[0] - dx - 0.0625,
+    0,
+    fc="black",
+    ec="black",
+    head_width=hw,
+    head_length=hl,
+    overhang=dx,
+    length_includes_head=True,
+    linewidth=0.5,
+    zorder=2,
+)
+plt.arrow(
+    dx,
+    yhat[1],
+    xhat[1] - dx - 0.0625,
+    0,
+    fc="black",
+    ec="black",
+    head_width=hw,
+    head_length=hl,
+    overhang=dx,
+    length_includes_head=True,
+    linewidth=0.5,
+    zorder=2,
+)
+
+# Save figure
+
+plt.savefig("us_md_montgomery.png", dpi=400, bbox_inches="tight")
