24100800cb51f0af2d4695ee87fdbef289938db6
Trevor Keller <trevor.keller@nist.gov>
2020-03-31 20:09:44 -0400

    Substitute *t* for *x*

--- README.md
+++ README.md
@@ -11,10 +11,11 @@ Ported from @psteinb's excellent chart for [Dresden, Germany](https://github.com
 
 The figure above is a least-squares fit to a power-law using the available data:
 
-```latex
-f(t) = a * (1 + b)^x
+```math
+f(t) = a * (1 + b)^t
 ```
 
+where *f* is the number of COVID-19 cases and *t* is time, in days since the first reported cases.
 The fitting process used in this analysis gives a covariance matrix for the model parameters (*a*
 and *b*). From the covariance matrix, it's possible to compute the one-standard-deviation (sigma)
 bounds on the parameters, assuming that the uncertainty on the number of COVID-19 cases for each day
--- exponential.py
+++ exponential.py
@@ -11,12 +11,12 @@ from scipy.stats import describe, t
 from matplotlib import style
 style.use("seaborn")
 
-def model(x, a, b):
-    return a * (1 + b) ** x
+def model(t, a, b):
+    return a * (1 + b) ** t
 
-def jacobian(x, a, b):
-    return np.array([(1 + b) ** x,
-                     (a * x * (1 + b) ** x) / (1 + b)]).T
+def jacobian(t, a, b):
+    return np.array([(1 + b) ** t,
+                     (a * t * (1 + b) ** t) / (1 + b)]).T
 
 
 fig = plt.figure(figsize=(6, 4))
@@ -35,31 +35,31 @@ start = date(start.tm_year, start.tm_mon, start.tm_mday).toordinal()
 today = strptime(df["date"].iloc[-1], "%Y-%m-%d")
 today = date(today.tm_year, today.tm_mon, today.tm_mday).toordinal()
 
-x = np.zeros_like(y)
-for i in range(len(x)):
+t = np.zeros_like(y)
+for i in range(len(t)):
     day = strptime(df["date"].iloc[i], "%Y-%m-%d")
-    x[i] = date(day.tm_year, day.tm_mon, day.tm_mday).toordinal() - start
+    t[i] = date(day.tm_year, day.tm_mon, day.tm_mday).toordinal() - start
 
-plt.scatter(x, y, marker=".", s=10, color="k", zorder=10)
+plt.scatter(t, y, marker=".", s=10, color="k", zorder=10)
 
 # Levenburg-Marquardt Least-Squares Fit
 """
 Note that sigma represents the relative error associated with each data point. By default, curve_fit
-will assume an array of opnes (constant values imply no difference in error), which is probably
+will assume an array of ones (constant values imply no difference in error), which is probably
 incorrect: given sparsity of testing, there's considerable uncertainty, and the earlier numbers may
 be lower than the truth to a greater extent than the later numbers. Quantifying this error by some
 non-trivial means for each datapoint would produce much more realistic uncertainty bands in the
 final plots.
 """
-popt, pcov = curve_fit(model, x, y, sigma=None, method="lm", jac=jacobian)
+popt, pcov = curve_fit(model, t, y, sigma=None, method="lm", jac=jacobian)
 perr = np.sqrt(np.diag(pcov))
 coef = describe(pcov)
 a, b = popt
 
 # Confidence Band: dfdp represents the partial derivatives of the model with respect to each parameter p (i.e., a and b)
 
-xhat = np.linspace(0, x[-1] + 7, 100)
-yhat = model(xhat, a, b)
+that = np.linspace(0, t[-1] + 7, 100)
+yhat = model(that, a, b)
 
 upr_a = a + perr[0]
 lwr_a = a - perr[0]
@@ -67,16 +67,16 @@ lwr_a = a - perr[0]
 upr_b = b + perr[1]
 lwr_b = b - perr[1]
 
-upper = model(xhat, upr_a, upr_b)
-lower = model(xhat, lwr_a, lwr_b)
+upper = model(that, upr_a, upr_b)
+lower = model(that, lwr_a, lwr_b)
 
-ix = np.argsort(xhat)
-plt.plot(xhat[ix], yhat[ix], c="red", lw=1, zorder=5)
+it = np.argsort(that)
+plt.plot(that[it], yhat[it], c="red", lw=1, zorder=5)
 plt.fill_between(
-    xhat[ix], upper[ix], yhat[ix], edgecolor=None, facecolor="silver", zorder=1
+    that[it], upper[it], yhat[it], edgecolor=None, facecolor="silver", zorder=1
 )
 plt.fill_between(
-    xhat[ix], lower[ix], yhat[ix], edgecolor=None, facecolor="silver", zorder=1
+    that[it], lower[it], yhat[it], edgecolor=None, facecolor="silver", zorder=1
 )
 
 # Predictions
@@ -87,21 +87,21 @@ dt = 14
 tomorrow = date.fromordinal(today + 1)
 nextWeek = date.fromordinal(today + 7)
 
-xhat = np.array([tomorrow.toordinal() - start, nextWeek.toordinal() - start])
-yhat = model(xhat, a, b)
+that = np.array([tomorrow.toordinal() - start, nextWeek.toordinal() - start])
+yhat = model(that, a, b)
 
-upper = model(xhat, upr_a, upr_b)
-lower = model(xhat, lwr_a, lwr_b)
+upper = model(that, upr_a, upr_b)
+lower = model(that, lwr_a, lwr_b)
 
 
 plt.text(0.5, (yhat[1] + 3 * upper[1]) / 4,
-         r"$y = ({0:.4f} \pm {2:.4f}) \times [1 + ({1:.4f} \pm {3:.4f})]^x$".format(a, b, perr[0], perr[1]),
+         r"$y = ({0:.4f} \pm {2:.4f}) \times [1 + ({1:.4f} \pm {3:.4f})]^t$".format(a, b, perr[0], perr[1]),
          zorder=5,
          bbox=dict(boxstyle="round", ec="black", fc="white", linewidth=2*dx)
 )
 
 plt.text(
-    xhat[0] - dt,
+    that[0] - dt,
     yhat[0],
     "{0}/{1}: ({2:.0f} < {3:.0f} < {4:.0f})".format(
         tomorrow.month, tomorrow.day, lower[0], yhat[0], upper[0]
@@ -111,7 +111,7 @@ plt.text(
     bbox=dict(boxstyle="round", ec="black", fc="white", linewidth=dx)
 )
 plt.text(
-    xhat[1] - dt,
+    that[1] - dt,
     yhat[1],
     "{0}/{1}: ({2:.0f} < {3:.0f} < {4:.0f})".format(
         nextWeek.month, nextWeek.day, lower[1], yhat[1], upper[1]
@@ -122,10 +122,10 @@ plt.text(
 )
 
 hw = 12
-hl = xhat[1] / 100
+hl = that[1] / 100
 
 plt.arrow(
-    xhat[0] - dt,
+    that[0] - dt,
     yhat[0],
     dt - dx + 0.0625,
     0,
@@ -139,7 +139,7 @@ plt.arrow(
     zorder=2,
 )
 plt.arrow(
-    xhat[1] - dt,
+    that[1] - dt,
     yhat[1],
     dt - dx + 0.0625,
     0,
@@ -155,7 +155,7 @@ plt.arrow(
 
 # Plot Boundaries
 
-plt.xlim([0, xhat[-1]])
+plt.xlim([0, that[-1]])
 plt.ylim([0, upper[-1]])
 
 # Save figure
