9947f949859e9c4ddf38214a3931f18d951ff63b
Trevor Keller <trevor.keller@gmail.com>
2020-06-25 10:42:04 -0400

    replace quotients of exp with cosh (via SymPy :-)

--- exponential.py
+++ exponential.py
@@ -6,16 +6,16 @@ columns = ["diagnosed", "killed"]
 
 # Specify which model to use: "exp" for exponential, "log" for logistic
 
-models = {"diagnosed": "log",
-          "killed": "log"}
+models = {"diagnosed": "log", "killed": "log"}
 
-equations = {"exp": "$f(t) = a (1 + b)^t$\n$a = {0:.4f} \pm {2:.5f}$\n$b = {1:.4f} \pm {3:.6f}$",
-             "log": "$f(t) = c / (1 + \exp((b - t)/a))$\n$a = {0:.3f} \pm {3:.4f}$\n$b = {1:.2f} \mp {4:.4f}$\n$c = {2:.0f}. \pm {5:.2f}$"}
+equations = {
+    "exp": "$f(t) = a (1 + b)^t$\n$a = {0:.4f} \pm {2:.5f}$\n$b = {1:.4f} \pm {3:.6f}$",
+    "log": "$f(t) = c / (1 + \exp((b - t)/a))$\n$a = {0:.3f} \pm {3:.4f}$\n$b = {1:.2f} \mp {4:.4f}$\n$c = {2:.0f}. \pm {5:.2f}$",
+}
 
 # Set colors for the plot
 
-colors = {"diagnosed": "red",
-          "killed": "black"}
+colors = {"diagnosed": "red", "killed": "black"}
 
 # Specify names for dataset (input) and plot (output)
 
@@ -24,11 +24,9 @@ imgname = "us_md_montgomery.png"
 
 # Prepare dicts for stats
 
-residuals = {"diagnosed": [],
-             "killed": []}
+residuals = {"diagnosed": [], "killed": []}
 
-chi_sq_red = {"diagnosed": 1.0,
-              "killed": 1.0}
+chi_sq_red = {"diagnosed": 1.0, "killed": 1.0}
 
 # Everything else is details
 
@@ -41,38 +39,55 @@ from string import Template
 from scipy.optimize import curve_fit
 from scipy.stats import describe, chisquare, t
 from matplotlib import style
+
 style.use("seaborn")
 
 # Define the model equation and its Jacobian
+
 
 def f_exp(t, a, b):
     # Exponential growth law, $f(t) = a * (1 + b) ^ t$,
     # where $a$ is the number of cases at $t=0$ and $b$ is the growth rate.
     return a * (1 + b) ** t
 
+
 def df_exp(t, a, b):
     # Jacobian: df/dp for p=(a, b)
-    return np.array([(1 + b) ** t,
-                     (a * t * (1 + b) ** t) / (1 + b)]).T
+    return np.array([(1 + b) ** t, (a * t * (1 + b) ** t) / (1 + b)]).T
+
 
 def f_log(t, a, b, c):
     # Logistic growth law, $f(t) = c / (exp((b - t)/a) + 1)$
-    return c / (np.exp((b - t)/a) + 1)
+    return c / (np.exp((b - t) / a) + 1)
+
 
 def df_log(t, a, b, c):
-    # Jacobian: df/dp for p=(a, b, c)
-    return np.array([c*(b - t)*np.exp((b - t)/a)/(a**2*(np.exp((b - t)/a) + 1)**2),
-                     -c*np.exp((b - t)/a)/(a*(np.exp((b - t)/a) + 1)**2),
-                     1/(np.exp((b - t)/a) + 1)]).T
+    # Jacobian: df/dp for p=(a, b, c)                              # SymPy-simplified quotients of exponentials
+    return np.array(
+        [
+            c
+            * (b - t)
+            / (
+                4 * a ** 2 * np.cosh((b - t) / (2 * a)) ** 2
+            ),  # c*(b - t)*np.exp((b - t)/a)/(a**2*(np.exp((b - t)/a) + 1)**2),
+            -c
+            / (
+                4 * a * np.cosh((b - t) / (2 * a)) ** 2
+            ),  # -c*np.exp((b - t)/a)/(a*(np.exp((b - t)/a) + 1)**2),
+            1 / (np.exp((b - t) / a) + 1),
+        ]
+    ).T
+
 
 def sigfig(x, n):
     # Round a float, x, to n significant figures.
     # Source: https://github.com/corriander/python-sigfig
     n = int(n)
 
-    e = np.floor(np.log10(np.abs(x)) - n + 1) # exponent, 10 ** e
-    shifted_dp = x / (10 ** e) # decimal place shifted n d.p.
-    return np.around(shifted_dp) * (10 ** e) # round and revert
+    e = np.floor(np.log10(np.abs(x)) - n + 1)  # exponent, 10 ** e
+    shifted_dp = x / (10 ** e)  # decimal place shifted n d.p.
+    return np.around(shifted_dp) * (10 ** e)  # round and revert
+
 
 fig = plt.figure(figsize=(6, 4))
 plt.suptitle("COVID-19 in Montgomery County, Maryland, USA", fontweight="bold")
@@ -94,15 +109,15 @@ today = date(today.tm_year, today.tm_mon, today.tm_mday).toordinal()
 
 t_max = 0
 y_max = 0
-y_off = 1 + 0.35 # offset for equation boxes
+y_off = 1 + 0.35  # offset for equation boxes
 
 for key in columns:
     model = models[key]
-    f  = f_exp
+    f = f_exp
     df = df_exp
 
     if model == "log":
-        f  = f_log
+        f = f_log
         df = df_log
 
     y = np.array(data[key])
@@ -112,7 +127,9 @@ for key in columns:
         day = strptime(data["date"].iloc[i], "%Y-%m-%d")
         t[i] = date(day.tm_year, day.tm_mon, day.tm_mday).toordinal() - start
 
-    plt.scatter(t, y, marker=".", s=10, color="white", edgecolors=colors[key], zorder=10)
+    plt.scatter(
+        t, y, marker=".", s=10, color="white", edgecolors=colors[key], zorder=10
+    )
 
     # Levenburg-Marquardt Least-Squares Fit
     """
@@ -141,18 +158,20 @@ for key in columns:
     t_hat = np.linspace(0, t[-1] + 7, 100)
     y_hat = f(t_hat, *p)
 
-    if (models[key] == "log"):
+    if models[key] == "log":
         perr[1] *= -1
     upr_p = p + perr
     lwr_p = p - perr
-    if (models[key] == "log"):
+    if models[key] == "log":
         perr[1] *= -1
 
     upper = f(t_hat, *upr_p)
     lower = f(t_hat, *lwr_p)
 
     it = np.argsort(t_hat)
-    plt.plot(t_hat[it], y_hat[it], c=colors[key], lw=1, zorder=5, label=key.capitalize())
+    plt.plot(
+        t_hat[it], y_hat[it], c=colors[key], lw=1, zorder=5, label=key.capitalize()
+    )
     plt.fill_between(
         t_hat[it], upper[it], y_hat[it], edgecolor=None, facecolor="silver", zorder=1
     )
@@ -179,13 +198,15 @@ for key in columns:
 
     # Overlay model on plot
     y_off -= 0.5
-    plt.text(1, y_off * dmax,
-             equations[models[key]].format(*s, *serr),
-             fontsize=7,
-             color=colors[key],
-             va="top",
-             zorder=4,
-             bbox=dict(boxstyle="round", ec="gray", fc="ghostwhite", linewidth=2.5*dx)
+    plt.text(
+        1,
+        y_off * dmax,
+        equations[models[key]].format(*s, *serr),
+        fontsize=7,
+        color=colors[key],
+        va="top",
+        zorder=4,
+        bbox=dict(boxstyle="round", ec="gray", fc="ghostwhite", linewidth=2.5 * dx),
     )
 
     if t_hat[-1] > t_max:
@@ -217,8 +238,13 @@ N = 5
 for key in columns:
     y = residuals[key]
     x = np.arange(0, len(y))
-    plt.bar(x, y, align="edge", color=colors[key],
-            label="{0}: $\\chi^2_\\nu={1:.3g}$".format(key.capitalize(), chi_sq_red[key]))
+    plt.bar(
+        x,
+        y,
+        align="edge",
+        color=colors[key],
+        label="{0}: $\\chi^2_\\nu={1:.3g}$".format(key.capitalize(), chi_sq_red[key]),
+    )
     """
     mov_avg = np.convolve(y, np.ones((N,))/N, mode='valid')
     x = np.arange(N-3, len(y)-(N-3))
@@ -242,16 +268,16 @@ key = "diagnosed"
 x = np.array(data[key])
 y = np.array(x)
 
-for i in np.arange(len(y)-1, 1, -1):
-    y[i] -= y[i-1]
+for i in np.arange(len(y) - 1, 1, -1):
+    y[i] -= y[i - 1]
 
 plt.scatter(x, y, s=8, color=colors[key], label=key.capitalize())
 
 key = "killed"
 y = np.array(data[key])
 
-for i in np.arange(len(y)-1, 1, -1):
-    y[i] -= y[i-1]
+for i in np.arange(len(y) - 1, 1, -1):
+    y[i] -= y[i - 1]
 
 plt.scatter(x, y, s=8, color=colors[key], label=key.capitalize())
 
@@ -260,4 +286,3 @@ axes = plt.gca()
 axes.set_ylim(ymin=0)
 plt.savefig("increment.png", dpi=400, bbox_inches="tight")
 plt.close()
-
