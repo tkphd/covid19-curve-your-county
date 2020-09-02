03c58738f600a27b914e3fcd9ba44d106c043dd9
Trevor Keller <trevor.keller@gmail.com>
2020-09-01 20:50:15 -0400

    2020-09-01 update

--- exponential.py
+++ exponential.py
@@ -105,7 +105,7 @@ months = [
     ["June",      date(2020,  6, 1).toordinal() - start],
     ["July",      date(2020,  7, 1).toordinal() - start],
     ["August",    date(2020,  8, 1).toordinal() - start],
-    # ["September", date(2020,  9, 1).toordinal() - start],
+    ["September", date(2020,  9, 1).toordinal() - start],
     # ["October",   date(2020, 10, 1).toordinal() - start],
     # ["November",  date(2020, 11, 1).toordinal() - start],
     # ["December",  date(2020, 12, 1).toordinal() - start]
